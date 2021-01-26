dbac566dedd651251836f4aea7cb75403bb0e58f
etopiei <lj343@icloud.com>
2021-01-26 21:48:19 +1100

    Starting off with a basic script that works.

---
+++ graph.py
@@ -0,0 +1,38 @@
+import plotly.graph_objects as go
+import json
+
+match_data = json.loads(open(input("Enter hinge match filename: ")).read())
+
+number_of_likes_sent = sum([1 for person in match_data if "like" in person])
+number_of_likes_received = len(match_data) - number_of_likes_sent
+
+total_likes = number_of_likes_sent + number_of_likes_received
+
+matches_from_like_sent = sum([1 for person in match_data if "match" in person and "like" in person])
+matches_from_like_receieved = sum([1 for person in match_data if "match" in person and "like" not in person])
+
+reject_from_like_sent = number_of_likes_sent - matches_from_like_sent
+reject_from_like_recieved = number_of_likes_received - matches_from_like_receieved
+
+meetups = sum([1 for person in match_data if "metup" in person])
+match_no_meet = matches_from_like_sent + matches_from_like_receieved - meetups
+
+figure = go.Figure(data=[go.Sankey(
+    node = dict(
+        pad = 4,
+        thickness = 6,
+        line = dict(color= "black", width = 0.1),
+        label = ["Likes", "Like Sent", "Like Received", "Matched", "Met Up", "Nope"],
+        color = "red"
+    ),
+    link = dict(
+        source = [0, 0, 1, 1, 2, 2, 3, 3], 
+        target = [1, 2, 3, 5, 3, 5, 4, 5], 
+        value = [number_of_likes_sent, number_of_likes_received, matches_from_like_sent, reject_from_like_sent, matches_from_like_receieved, reject_from_like_recieved, meetups, match_no_meet]
+        ))])
+
+figure.update_layout(title_text="Hinge Likes", font_size=12)
+figure.show()
+
+
+
