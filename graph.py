c0d4c3bad54edb776242b0ae0269ff22a30e7c23
etopiei <lj343@icloud.com>
2021-02-01 22:49:33 +1100

    Make graph a little better with different connection order.

--- graph.py
+++ graph.py
@@ -14,7 +14,7 @@ matches_from_like_receieved = sum([1 for person in match_data if "match" in pers
 reject_from_like_sent = number_of_likes_sent - matches_from_like_sent
 reject_from_like_recieved = number_of_likes_received - matches_from_like_receieved
 
-meetups = sum([1 for person in match_data if "metup" in person])
+meetups = sum([1 for person in match_data if "we_met" in person])
 match_no_meet = matches_from_like_sent + matches_from_like_receieved - meetups
 
 figure = go.Figure(data=[go.Sankey(
@@ -22,12 +22,12 @@ figure = go.Figure(data=[go.Sankey(
         pad = 4,
         thickness = 6,
         line = dict(color= "black", width = 0.1),
-        label = ["Likes", "Like Sent", "Like Received", "Matched", "Met Up", "Nope"],
+        label = ["Likes", "Like Sent", "Like Received", "No Match", "Matched", "Met Up", "No Meet Up"],
         color = "red"
     ),
     link = dict(
-        source = [0, 0, 1, 1, 2, 2, 3, 3], 
-        target = [1, 2, 3, 5, 3, 5, 4, 5], 
+        source = [0, 0, 1, 1, 2, 2, 4, 4], 
+        target = [1, 2, 4, 3, 4, 3, 5, 6], 
         value = [number_of_likes_sent, number_of_likes_received, matches_from_like_sent, reject_from_like_sent, matches_from_like_receieved, reject_from_like_recieved, meetups, match_no_meet]
         ))])
 
